513d37b10720dadac6983a7b5a819bd5eeeb0dbb
Kishlin <hello@pierrelouislegrand.com>
2022-01-25 15:08:58 +0100

    Feature: Increment the Character Counter on character creation

---
+++ src/Backend/RPGIdleGame/CharacterCount/Application/OnCharacterCreated/CharacterCountNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kishlin\Backend\RPGIdleGame\CharacterCount\Application\OnCharacterCreated;
+
+use RuntimeException;
+
+final class CharacterCountNotFoundException extends RuntimeException
+{
+}
