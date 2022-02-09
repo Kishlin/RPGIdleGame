0e0b250579692a14f0b5baf90f1fb80a9015c508
Kishlin <hello@pierrelouislegrand.com>
2022-02-09 19:58:42 +0100

    Test: add a test to verify we can save an updated count

--- tests/Backend/ContractTests/RPGIdleGame/CharacterCount/Infrastructure/Persistence/Doctrine/CharacterCountRepositoryTest.php
+++ tests/Backend/ContractTests/RPGIdleGame/CharacterCount/Infrastructure/Persistence/Doctrine/CharacterCountRepositoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kishlin\Tests\Backend\ContractTests\RPGIdleGame\CharacterCount\Infrastructure\Persistence\Doctrine;
 
+use Doctrine\DBAL\Exception;
 use Kishlin\Backend\RPGIdleGame\CharacterCount\Domain\CharacterCount;
 use Kishlin\Backend\RPGIdleGame\CharacterCount\Infrastructure\Persistence\Doctrine\CharacterCountRepository;
 use Kishlin\Tests\Backend\Tools\Provider\CharacterCountProvider;
@@ -24,5 +25,29 @@ final class CharacterCountRepositoryTest extends RepositoryContractTestCase
         $repository->save($characterCount);
 
         self::assertCount(1, self::entityManager()->getRepository(CharacterCount::class)->findAll());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testItCanSaveAndUpdatedCharacterCount(): void
+    {
+        $characterCount = CharacterCountProvider::freshCount();
+        self::loadFixtures($characterCount);
+
+        $repository = new CharacterCountRepository(self::entityManager());
+
+        $savedCharacterCount = $repository->findForOwner($characterCount->owner());
+
+        self::assertNotNull($savedCharacterCount);
+
+        $savedCharacterCount->incrementOnCharacterCreation();
+
+        $repository->save($savedCharacterCount);
+
+        self::assertEquals(1, self::entityManager()->getConnection()->fetchOne(
+            'SELECT 1 FROM character_counts WHERE owner_id = :owner LIMIT 1',
+            ['owner' => $characterCount->owner()->value()],
+        ));
     }
 }
