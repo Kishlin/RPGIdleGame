b2e3adb46061521e4de1043940c20b7945178c5c
Kishlin <hello@pierrelouislegrand.com>
2022-02-12 22:25:33 +0100

    Maintenability: Split the Account Context into smaller classes

--- apps/RPGIdleGame/backend/tests/ApiTests/Context/AccountAuthenticationContext.php
+++ apps/RPGIdleGame/backend/tests/ApiTests/Context/AccountAuthenticationContext.php
@@ -7,45 +7,10 @@ namespace Kishlin\Tests\Apps\RPGIdleGame\Backend\ApiTests\Context;
 use Kishlin\Tests\Apps\RPGIdleGame\Backend\ApiTests\HTTPClient\Request;
 use PHPUnit\Framework\Assert;
 
-final class AccountContext extends RPGIdleGameAPIContext
+final class AccountAuthenticationContext extends RPGIdleGameAPIContext
 {
     private const REFRESH_TOKEN_WITH_NO_EXPIRATION = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjNmRjM2ZkOTNmNTYiLCJhdWQiOiJjNmRjM2ZkOTNmNTYiLCJpYXQiOjE2NDQ0MTczNDYsInVzZXIiOiI3ZDM4Nzc0MC01YzE1LTQ3MTItYmRjZi01MTI2YzI4ZmMxMGEiLCJzYWx0IjoiMWM5Y2YzZDM1YzZhMmFhYTY4NWEzOWRmY2JmMzRlNGRjMzgxMzJiNCJ9.4lfYorjgyPRFtvCe7OvSffgycSm_vPO0swxR5wmonh4';
     private const REFRESH_TOKEN_EXPIRED            = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjNmRjM2ZkOTNmNTYiLCJhdWQiOiJjNmRjM2ZkOTNmNTYiLCJpYXQiOjE2NDQ0MTczNDYsInVzZXIiOiI3ZDM4Nzc0MC01YzE1LTQ3MTItYmRjZi01MTI2YzI4ZmMxMGEiLCJleHAiOjE2NDQ0MTczNDYsInNhbHQiOiIxYzljZjNkMzVjNmEyYWFhNjg1YTM5ZGZjYmYzNGU0ZGMzODEzMmI0In0.B4Oayevt44xPOJQJN30kmFnVmd630P1dGTz6Cwfzwas';
-
-    /**
-     * @Given /^a client has an account$/
-     * @Given /^an account already exists with the email$/
-     *
-     * @noinspection SpellCheckingInspection
-     */
-    public function aClientHasAnAccount(): void
-    {
-        self::database()->exec(
-            <<<'SQL'
-INSERT INTO accounts (id, username, email, password, salt, is_active)
-VALUES ('7d387740-5c15-4712-bdcf-5126c28fc10a', 'User', 'user@example.com', '$argon2i$v=19$m=65536,t=4,p=1$LkdneGtiWW1ZaXJIM3QxNg$u4J11lsQJLZDbrj0B5qpeMSzm7ST/ES7Pk+NJ3M2LMU', '1c9cf3d35c6a2aaa685a39dfcbf34e4dc38132b4', true);
-
-INSERT INTO character_counts (owner_id, character_count, reached_limit)
-VALUES ('7d387740-5c15-4712-bdcf-5126c28fc10a', 0, false);
-SQL
-        );
-    }
-
-    /**
-     * @When /^a client creates an account$/
-     * @When /^a client creates an account with the same email$/
-     */
-    public function aClientCreatesAnAccount(): void
-    {
-        $this->response = self::client()->post(new Request(
-            uri: '/account/signup',
-            headers: [
-                'Content-Type: application/json',
-                'Authorization: Basic ' . base64_encode('User:password'),
-            ],
-            params: ['email' => 'user@example.com']
-        ));
-    }
 
     /**
      * @When /^a client authenticates with the correct credentials$/
@@ -90,36 +55,6 @@ SQL
     }
 
     /**
-     * @Then /^its credentials are registered$/
-     */
-    public function itsCredentialsAreRegistered(): void
-    {
-        Assert::assertNotNull($this->response);
-        Assert::assertSame(201, $this->response->httpCode());
-
-        /** @var array<string, mixed> $decodedBody */
-        $decodedBody = $this->response->decodedBody();
-
-        Assert::assertIsArray($decodedBody);
-        Assert::assertArrayHasKey('accountId', $decodedBody);
-
-        $count = self::database()->fetchOne(
-            'SELECT count(1) FROM accounts WHERE id = :id',
-            ['id' => $decodedBody['accountId']],
-        );
-
-        Assert::assertSame(1, $count);
-    }
-
-    /**
-     * @Then /^a fresh character count is registered$/
-     */
-    public function aFreshCharacterCountIsRegistered(): void
-    {
-        Assert::assertSame(1, self::database()->fetchOne('SELECT count(1) FROM character_counts'));
-    }
-
-    /**
      * @Then /^the authentication was authorized$/
      */
     public function theAuthenticationWasAuthorized(): void
@@ -157,15 +92,5 @@ SQL
     {
         Assert::assertNotNull($this->response);
         Assert::assertSame(401, $this->response->httpCode());
-    }
-
-    /**
-     * @Then /^it did not register the new account$/
-     */
-    public function itDidNotRegisterTheNewAccount(): void
-    {
-        Assert::assertNotNull($this->response);
-        Assert::assertSame(409, $this->response->httpCode());
-        Assert::assertSame(1, self::database()->fetchOne('SELECT count(1) FROM accounts')); // The account already using the email
     }
 }
---
+++ apps/RPGIdleGame/backend/tests/ApiTests/Context/AccountSignupContext.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kishlin\Tests\Apps\RPGIdleGame\Backend\ApiTests\Context;
+
+use Kishlin\Tests\Apps\RPGIdleGame\Backend\ApiTests\HTTPClient\Request;
+use PHPUnit\Framework\Assert;
+
+final class AccountSignupContext extends RPGIdleGameAPIContext
+{
+    /**
+     * @Given /^a client has an account$/
+     * @Given /^an account already exists with the email$/
+     *
+     * @noinspection SpellCheckingInspection
+     */
+    public function aClientHasAnAccount(): void
+    {
+        self::database()->exec(
+            <<<'SQL'
+INSERT INTO accounts (id, username, email, password, salt, is_active)
+VALUES ('7d387740-5c15-4712-bdcf-5126c28fc10a', 'User', 'user@example.com', '$argon2i$v=19$m=65536,t=4,p=1$LkdneGtiWW1ZaXJIM3QxNg$u4J11lsQJLZDbrj0B5qpeMSzm7ST/ES7Pk+NJ3M2LMU', '1c9cf3d35c6a2aaa685a39dfcbf34e4dc38132b4', true);
+
+INSERT INTO character_counts (owner_id, character_count, reached_limit)
+VALUES ('7d387740-5c15-4712-bdcf-5126c28fc10a', 0, false);
+SQL
+        );
+    }
+
+    /**
+     * @When /^a client creates an account$/
+     * @When /^a client creates an account with the same email$/
+     */
+    public function aClientCreatesAnAccount(): void
+    {
+        $this->response = self::client()->post(new Request(
+            uri: '/account/signup',
+            headers: [
+                'Content-Type: application/json',
+                'Authorization: Basic ' . base64_encode('User:password'),
+            ],
+            params: ['email' => 'user@example.com']
+        ));
+    }
+
+    /**
+     * @Then /^its credentials are registered$/
+     */
+    public function itsCredentialsAreRegistered(): void
+    {
+        Assert::assertNotNull($this->response);
+        Assert::assertSame(201, $this->response->httpCode());
+
+        /** @var array<string, mixed> $decodedBody */
+        $decodedBody = $this->response->decodedBody();
+
+        Assert::assertIsArray($decodedBody);
+        Assert::assertArrayHasKey('accountId', $decodedBody);
+
+        $count = self::database()->fetchOne(
+            'SELECT count(1) FROM accounts WHERE id = :id',
+            ['id' => $decodedBody['accountId']],
+        );
+
+        Assert::assertSame(1, $count);
+    }
+
+    /**
+     * @Then /^a fresh character count is registered$/
+     */
+    public function aFreshCharacterCountIsRegistered(): void
+    {
+        Assert::assertSame(1, self::database()->fetchOne('SELECT count(1) FROM character_counts'));
+    }
+
+    /**
+     * @Then /^it did not register the new account$/
+     */
+    public function itDidNotRegisterTheNewAccount(): void
+    {
+        Assert::assertNotNull($this->response);
+        Assert::assertSame(409, $this->response->httpCode());
+        Assert::assertSame(1, self::database()->fetchOne('SELECT count(1) FROM accounts')); // The account already using the email
+    }
+}
