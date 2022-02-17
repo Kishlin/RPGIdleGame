a595f7cb3c1a2272acfe3fe3ac65a59a28c49b50
Kishlin <hello@pierrelouislegrand.com>
2022-02-17 04:50:30 +0100

    Security: Return an HTTP 4O1 when the required token cookie is missing

--- apps/RPGIdleGame/backend/src/Errors/ExceptionHandlers/Security/ParsingTokenFailedExceptionHandler.php
+++ apps/RPGIdleGame/backend/src/Errors/ExceptionHandlers/Security/ParsingTokenFailedExceptionHandler.php
@@ -6,6 +6,7 @@ namespace Kishlin\Apps\RPGIdleGame\Backend\Errors\ExceptionHandlers\Security;
 
 use Kishlin\Apps\RPGIdleGame\Backend\Errors\KernelExceptionHandler;
 use Kishlin\Backend\Shared\Domain\Security\ParsingTokenFailedException;
+use Kishlin\Backend\Shared\Infrastructure\Security\Authorization\FailedToReadCookieException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
@@ -14,7 +15,7 @@ final class ParsingTokenFailedExceptionHandler implements KernelExceptionHandler
 {
     public function handle(Throwable $e): ?Response
     {
-        if ($e instanceof ParsingTokenFailedException) {
+        if ($e instanceof ParsingTokenFailedException || $e instanceof FailedToReadCookieException) {
             return new JsonResponse('Authentication failed.', Response::HTTP_UNAUTHORIZED);
         }
 
