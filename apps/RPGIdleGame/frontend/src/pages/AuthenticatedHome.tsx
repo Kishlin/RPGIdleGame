e4b4775271e2a17da56e9b9e9e73509fba35ae06
Kishlin <hello@pierrelouislegrand.com>
2022-02-21 15:20:48 +0100

    Feature: Refresh characters when loading the front page
    This makes sure we have up-to-date data of all characters, in case they had fights since the last time.

--- apps/RPGIdleGame/frontend/src/pages/AuthenticatedHome.tsx
+++ apps/RPGIdleGame/frontend/src/pages/AuthenticatedHome.tsx
@@ -1,19 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Container, Stack, Typography } from '@mui/material';
+
+import { UserContext } from '../context/UserContext';
+import { LangContext } from '../context/LangContext';
 
 import LayoutAuthenticated from '../components/Layout/LayoutAuthenticated';
 import NavigationButton from '../components/Navigation/NavigationButton';
 import CharactersList from '../components/Character/CharactersList';
 
 import useAuthenticatedPage from '../hooks/useAuthenticatedPage';
-import { UserContext } from '../context/UserContext';
-import { LangContext } from '../context/LangContext';
+import getAllCharactersUsingFetch from '../api/character/allCharacters';
 
 function AuthenticatedHome(): JSX.Element {
     useAuthenticatedPage();
 
-    const { characterCreationIsAllowed } = useContext<UserContextType>(UserContext);
+    const { characterCreationIsAllowed, setCharactersFromArray } = useContext<UserContextType>(UserContext);
     const { t } = useContext<LangContextType>(LangContext);
+
+    useEffect(
+        () => getAllCharactersUsingFetch(setCharactersFromArray),
+        [],
+    );
 
     const characterCreation = characterCreationIsAllowed()
         ? <NavigationButton text="pages.home.authenticated.buttons.createCharacter" to="/character/new" />
