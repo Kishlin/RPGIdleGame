155b803476d858857ddf168f08d59a8642f3af8e
Kishlin <hello@pierrelouislegrand.com>
2022-02-28 13:16:15 +0100

    Documentation: Add migration description

--- etc/Migrations/Version20220207181914.php
+++ etc/Migrations/Version20220207181914.php
@@ -7,14 +7,11 @@ namespace Kishlin\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20220207181914 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add stats column to the characters table.';
     }
 
     public function up(Schema $schema): void
