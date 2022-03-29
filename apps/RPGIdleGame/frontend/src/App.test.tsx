06d2e37a8a00ddc5832ee1910d13103950b03436
Kishlin <hello@pierrelouislegrand.com>
2022-03-29 14:09:08 +0200

    Hotfix: updated frontend test placeholder

--- apps/RPGIdleGame/frontend/src/App.test.tsx
+++ apps/RPGIdleGame/frontend/src/App.test.tsx
@@ -1,10 +1,36 @@
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
-test('renders learn react link', () => {
-    const { container } = render(<App />);
+jest.mock('./api/character/allCharacters', (): AllCharactersApi => {
+    return (onResponse) => onResponse([]);
+});
+
+let container: HTMLElementTagNameMap['div'] = null;
+
+beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+test('renders the loader on first mount', () => {
+    act(() => {
+        render(
+            <App />,
+            container,
+        );
+    })
+
     const linkElement = container.querySelector('div.loader');
     expect(linkElement).toBeInTheDocument();
 });
